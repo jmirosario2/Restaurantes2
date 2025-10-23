ed7a7baf7d07e1594ec4844536be1af1bb1d762a
jmirosario2 <jmirosario2@hotmail.com>
2025-10-23 15:04:44 -0400

    para verificar

--- src/components/Ordenar/RealizarOrden.vue
+++ src/components/Ordenar/RealizarOrden.vue
@@ -6,14 +6,14 @@
                 size="is-large"
                 type="is-primary">
             </b-icon>
-            Realizar orden 
+            Realizar orden
         </p>
 
         <div class="columns is-multiline">
-            <div class="column" 
-            v-for="mesa in mesas" 
+            <div class="column"
+            v-for="mesa in mesas"
             :key="mesa.mesa.idMesa">
-                
+
                 <div class="box">
                     <p class="title is-2 has-text-grey">Mesa #{{ mesa.mesa.idMesa }}
                         <span class="title is-1 has-text-weight-bold is-pulled-right" v-if="mesa.mesa.total">
@@ -26,9 +26,9 @@
                     <p v-if="mesa.mesa.cliente">
                         <strong>Cliente</strong>: {{ mesa.mesa.cliente }}
                     </p>
-                    <b-collapse 
-                        class="card" 
-                        animation="slide" 
+                    <b-collapse
+                        class="card"
+                        animation="slide"
                         aria-id="contentIdForA11y3"
                         v-if="mesa.mesa.estado === 'ocupada'">
                         <template #trigger="props">
@@ -60,34 +60,34 @@
 
                                     <b-table-column field="codigo" label="Código" v-slot="props">
                                         {{ props.row.codigo }}
-                                    </b-table-column>                                    
-                                    
+                                    </b-table-column>
+
                                     <b-table-column field="nombre" label="Nombre" v-slot="props">
                                         {{ props.row.nombre }}
-                                    </b-table-column>                                    
-                                    
+                                    </b-table-column>
+
                                     <b-table-column field="caracteristicas" label="Características" v-slot="props">
                                         {{ props.row.caracteristicas }}
-                                    </b-table-column>  
+                                    </b-table-column>
 
                                     <b-table-column field="cantidad" label="Cantidad" v-slot="props">
                                         {{ props.row.cantidad }} X ${{  props.row.precio }}
-                                    </b-table-column>                                        
-                                    
+                                    </b-table-column>
+
                                     <b-table-column field="subtotal" label="Subtotal" v-slot="props">
                                         ${{ props.row.cantidad * props.row.precio }}
-                                    </b-table-column>                                                                
+                                    </b-table-column>
                                     <b-table-column field="estado" label="" v-slot="props">
                                         <b-icon icon="alert" type="is-danger" v-if="props.row.estado ==='pendiente'"></b-icon>
                                         <b-icon icon="check" type="is-success" v-if="props.row.estado ==='entregado'"></b-icon>
-                                    </b-table-column>                                                                
+                                    </b-table-column>
                                 </b-table>
                             </div>
                         </div>
                     </b-collapse>
                     <br>
                     <div class="has-text-centered">
-                        <b-button type="is-primary" icon-left="check" @click="ocuparMesa(mesa)" v-if="mesa.mesa.estado === 'libre'">Ocupar</b-button>  
+                        <b-button type="is-primary" icon-left="check" @click="ocuparMesa(mesa)" v-if="mesa.mesa.estado === 'libre'">Ocupar</b-button>
                         <div class="field is-grouped is-centered" v-if="mesa.mesa.estado === 'ocupada'">
                             <p class="control">
                                 <b-button type="is-success" icon-left="cash" @click="cobrar(mesa)">Cobrar</b-button>
@@ -102,7 +102,7 @@
                                 <b-button type="is-danger" icon-left="close"  @click="cancelarOrden(mesa.mesa.idMesa)">Cancelar</b-button>
                             </p>
                         </div>
-                    </div>            
+                    </div>
                 </div>
             </div>
         </div>
@@ -160,7 +160,7 @@ export default ({
                             this.cargando = false
                         }
                     })
-                    
+
                 }
             })
         },
@@ -259,12 +259,12 @@ export default ({
                         idMesa: mesa.mesa.idMesa,
                         cliente: mesa.mesa.cliente,
                         total: mesa.mesa.total,
-                        pagado: value, 
+                        pagado: value,
                         idUsario: mesa.mesa.idUsuario,
                         insumos: mesa.insumos,
                         atiende: mesa.mesa.atiende
                     }
-                    
+
                     HttpService.registrar(payload, "registrar_venta.php")
                     .then(registrado => {
                         if(registrado){
@@ -278,7 +278,7 @@ export default ({
                             this.cargando = false
                         }
                     })
-                    
+
                 }
             })
         },
@@ -292,12 +292,17 @@ export default ({
             })
         },
 
-        ocuparMesa(mesa){
-            this.$router.push({
-                name: "Ordenar",
-                params: { id: mesa.mesa.idMesa, insumosEnLista: mesa.insumos, cliente: mesa.mesa.cliente },
-            })
-        }
-    }
-})
+
+  ocuparMesa(mesa) {
+    this.$router.push({
+      name: "Ordenar",
+      params: {
+        id: mesa.mesa.idMesa,
+        insumosEnLista: mesa.insumos,
+        cliente: mesa.mesa.cliente
+      }
+    });
+  }
+}
+
 </script>
